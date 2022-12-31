d22ef92d4bf7d0be2f22fdec99a49fe513fd59a0
monicahardt <monha@itu.dk>
2022-12-31 23:19:06 +0100

    Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Peer2Peer
+
+To run this code start three different peers on ports 5001, 5002 and 5003
+as so
+
+go run . -port 5001
+go run . -port 5002
+go run . -port 5003
+
+pres enter at a terminal to request access to the critical section
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	proto "Peer2peer/grpc"
 	"bufio"
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -10,10 +12,22 @@ import (
 	"strconv"
 	"time"
 
-	proto "Peer2peer/grpc"
-
 	"google.golang.org/grpc"
 )
+
+type Peer struct {
+	proto.UnimplementedReceiveServer
+	id               int32
+	lamport          int32
+	amountOfRequests map[int32]int32
+	state            State
+	clients          map[int32]proto.ReceiveClient
+	ctx              context.Context
+	port 			int
+}
+
+var criticalSection = false;
+var port = flag.Int("port", 0, "server port number")
 
 func main() {
 	//setting the log file
@@ -24,72 +38,85 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 	
-	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
-	ownPort := int32(arg1) + 5000
+	// arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
+	// ownPort := int32(arg1) + 5000
+
+	flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	p := &peer{
-		id:               ownPort,
+	p := &Peer{
+		id:               int32(*port),
+		port: 			*port,
 		amountOfRequests: make(map[int32]int32),
 		clients:          make(map[int32]proto.ReceiveClient),
 		ctx:              ctx,
 	}
 
-	// Create listener tcp on port ownPort
-	list, err := net.Listen("tcp", "localhost"+fmt.Sprintf(":%v", ownPort))
-	if err != nil {
-		log.Fatalf("Failed to listen on port: %v", err)
+	go startPeerServer(p)
+	go p.connectToOtherPeers()
+
+	for {
+
 	}
-	grpcServer := grpc.NewServer()
-	receive.RegisterReceiveServer(grpcServer, p)
+}
 
-	go func() {
-		if err := grpcServer.Serve(list); err != nil {
-			log.Fatalf("failed to server %v", err)
-		}
-	}()
+func startPeerServer(p *Peer) {
+
+	grpcServer := grpc.NewServer()                                           // create a new grpc server
+	listen, err := net.Listen("tcp", "localhost:"+strconv.Itoa(p.port)) // creates the listener
+
+	if err != nil {
+		log.Fatalln("Could not start listener")
+	}
+
+	log.Printf("Started peer at port %v", p.port)
+	fmt.Printf("Started peer at port %v", p.port)
+
+	proto.RegisterReceiveServer(grpcServer, p)
+	serverError := grpcServer.Serve(listen)
+
+	if serverError != nil {
+		log.Printf("Could not register server")
+	}
+
+}
+
+func (p *Peer) connectToOtherPeers(){
+	//hardcoded to connect to peers with ports 5001, 5002 and 5003
 	for i := 0; i < 3; i++ {
-		port := int32(5000) + int32(i)
+		port := int32(5001) + int32(i)
 
-		if port == ownPort {
+		//if it is the peers own port we just continue
+		if port == int32(p.port) {
 			continue
 		}
 
 		var conn *grpc.ClientConn
 		log.Printf("Trying to dial: %v\n", port)
 		fmt.Printf("Trying to dial: %v\n", port)
+
 		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
 			log.Fatalf("Could not connect: %s", err)
 		}
-		defer conn.Close()
-		c := receive.NewReceiveClient(conn)
-		p.clients[port] = c
-	}
 
+		defer conn.Close()
+		c := proto.NewReceiveClient(conn)
+		//adding the new connection to another peer (client) to the map of peers/clients
+		p.clients[port] = c
+		fmt.Printf("Connection made to peer with id: %v\n", port)
+	}
+	//wait for someone to want to enter
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		p.Enter()
 	}
 }
 
-type peer struct {
-	receive.UnimplementedReceiveServer
-	id               int32
-	lamport          int32
-	amountOfRequests map[int32]int32
-	state            State
-	clients          map[int32]receive.ReceiveClient
-	ctx              context.Context
-	deferQueue		 []int32
 
-}
-
-var criticalSection = false;
-
-func (p *peer) Receive(ctx context.Context, req *receive.Request) (*receive.Reply, error) {
+func (p *Peer) Receive(ctx context.Context, req *proto.Request) (*proto.Reply, error) {
 	if(p.lamport < req.Lamport) { p.lamport = req.Lamport; }
 	p.lamport++;
 
@@ -98,21 +125,21 @@ func (p *peer) Receive(ctx context.Context, req *receive.Request) (*receive.Repl
 			time.Sleep(2*time.Second);
 		}
 		p.lamport++;
-		rep := &receive.Reply{Id: p.id, Lamport: p.lamport};
+		rep := &proto.Reply{Id: p.id, Lamport: p.lamport};
 		return rep, nil;
 	} else {
 		p.lamport++;
-		rep := &receive.Reply{Id: p.id, Lamport: p.lamport};
+		rep := &proto.Reply{Id: p.id, Lamport: p.lamport};
 		return rep, nil;
 	}
 }
 
-func (p *peer) Enter() {
+func (p *Peer) Enter() {
 	p.state = wanted
 	for _, client := range p.clients {
 		p.lamport++;
 		log.Printf("Peer with id: %v requested access to the critical section with lamporttime: %v", p.id,p.lamport)
-		request := &receive.Request{Id: p.id, Lamport: p.lamport}
+		request := &proto.Request{Id: p.id, Lamport: p.lamport}
 		rep,_:= client.Receive(p.ctx, request)
 		if(rep.Lamport > p.lamport) {p.lamport = rep.Lamport}
 		p.lamport++;
@@ -128,7 +155,7 @@ func (p *peer) Enter() {
 	p.Exit();
 }
 
-func (p *peer) Exit(){
+func (p *Peer) Exit(){
 	p.lamport++;
 	p.state = released
 	criticalSection = false;
